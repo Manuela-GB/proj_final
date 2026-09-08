1e5110c16a298b7c17ad3376fa9ca6b86823b04e
Manuela <llhpmagb@gmail.com>
2026-09-08 17:08:52 -0300

    newcode

--- main.py
+++ main.py
@@ -2,29 +2,128 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-dict_news = {"news": [], "links": [], "views": [], "comments": []}
+# DICIONÁRIO
+dict_news = {
+    "news": [],
+    "links": [],
+    "category": [],
+    "author": [],
+    "date": [],
+    "summary": []
+}
 
-url = "https://revistapesquisa.fapesp.br/"
+# PÁGINA DA REVSITA
+url = "https://periodicos.upe.br/index.php/revistaescripturas"
 
-pages = [""]  # página principal
+response = requests.get(url)
+response.raise_for_status()
 
-for i in pages:
-    response = requests.get(url + i)
-    bs = BeautifulSoup(response.text, "lxml")
+bs = BeautifulSoup(response.text, "lxml")
 
-    # Aqui buscamos todos os links das notícias
-    temp = bs.select("h2 a")
+# INÍCIO DA TABELA
 
-    for post in temp:
-        dict_news["news"].append(post.text.strip())
-        dict_news["links"].append(post.get("href"))
+# 1: LINKS
+links = bs.select('a[href*="/article/view/"]')
 
-        # O site não possui visualizações e comentários visíveis na listagem
-        dict_news["views"].append("N/A")
-        dict_news["comments"].append("N/A")
+article_links = []
 
-df_news = pd.DataFrame(dict_news, columns=["news", "links", "views", "comments"])
+for link in links:
+    href = link.get("href")
 
-df_news.to_csv("./saved_data.csv", index=False)
+    if href and href not in article_links:
+        article_links.append(href)
 
+print("Quantidade de artigos encontrados:",
+      len(article_links))
+
+# PEGAR CADA ARTIGO
+for link in article_links:
+    if link.startswith("/"):
+        article_url = "https://periodicos.upe.br" + link
+    else:
+        article_url = link
+
+    # página do artigo
+    article_response = requests.get(article_url)
+    article_response.raise_for_status()
+
+    article_bs = BeautifulSoup(
+        article_response.text,
+        "lxml"
+    )
+
+    # 3: TÍTULO
+    title = article_bs.select_one("h1")
+
+    if title:
+        title = title.get_text(" ", strip=True)
+    else:
+        title = "N/A"
+
+    # 4: AUTORES
+    authors = article_bs.select(".authors .name")
+
+    if authors:
+        author = ", ".join(
+            a.get_text(" ", strip=True)
+            for a in authors
+        )
+    else:
+        author = "N/A"
+
+    # 5: RESUMO
+    resumo = article_bs.select_one(".resumo")
+
+    if resumo:
+        summary = resumo.get_text(" ", strip=True)
+    else:
+        summary = "N/A"
+
+    # 6: DATA
+    date = article_bs.select_one(
+        ".item.published .value"
+    )
+    if date:
+        date = date.get_text(" ", strip=True)
+    else:
+        date = "N/A"
+
+    # 7: CATEGORIA
+    category = article_bs.select_one(
+        ".item.section .value"
+    )
+
+    if category:
+        category = category.get_text(" ", strip=True)
+    else:
+        category = "N/A"
+
+    # DICIONÁRIO (ADD)
+    dict_news["news"].append(title)
+    dict_news["links"].append(article_url)
+    dict_news["category"].append(category)
+    dict_news["author"].append(author)
+    dict_news["date"].append(date)
+    dict_news["summary"].append(summary)
+
+
+# DATAFRAME
+df_news = pd.DataFrame(
+    dict_news,
+    columns=[
+        "news",
+        "links",
+        "category",
+        "author",
+        "date",
+        "summary"
+    ]
+)
+
+df_news.to_csv(
+    "./saved_data.csv",
+    index=False
+)
+
+# RESULTADOS
 df_news.head()
